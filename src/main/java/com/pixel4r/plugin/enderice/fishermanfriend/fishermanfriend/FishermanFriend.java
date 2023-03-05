2ea2131aba039e75926bc45ad61a4959636a59ce
Francis Chen <tuangchok.ch@protonmail.com>
2023-03-05 17:21:00 +0700

    init paper plugin

---
+++ src/main/java/com/pixel4r/plugin/enderice/fishermanfriend/fishermanfriend/FishermanFriend.java
@@ -0,0 +1,17 @@
+package com.pixel4r.plugin.enderice.fishermanfriend.fishermanfriend;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FishermanFriend extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
